f315ec17f7e6a0ea92d1a79b7f0fa1f455cec680
yuriohliveira <61686424+yuriohliveira@users.noreply.github.com>
2020-05-13 14:15:52 -0700

    first push

---
+++ orderup/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Home() {
+    return (
+      <div>
+        <h1>Welcome visitor</h1>
+        <h4>This is the HOME page</h4>
+      </div>
+    );
+  }
+  
+  export default Home;
---
+++ orderup/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Login() {
+    return (
+      <div>
+        <h1>Welcome visitor</h1>
+        <h4>This is the LOG IN page</h4>
+      </div>
+    );
+  }
+  
+  export default Login;
